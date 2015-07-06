9c2b5acd5bd35588b7f7406f019b4bbada106dee
BooMamoo <yanakorn_eve@hotmail.com>
2015-07-06 13:20:44 +0700

    limit num of pagination

--- public/js/CheckController.js
+++ public/js/CheckController.js
@@ -6,14 +6,73 @@ app.controller("CheckController", function($scope, $http, $routeParams)
 	.success(function(response) {
 		$scope.checks = response.checks;
 		$scope.room = response.room;
-		$scope.total_page = [];
-		$scope.pageNo = $routeParams.page;
-		$scope.lastPage = response.total_page;
 		$scope.status = response.status;
 
-		for(var i = 1 ; i <= response.total_page ; i++)
+		$scope.total_page = [];
+		$scope.pageNo = parseInt($routeParams.page);
+		$scope.lastPage = parseInt(response.total_page);
+
+		var lenPage = 3;
+
+		function sortNumber(a, b) 
 		{
-			$scope.total_page.push(i);
+		    return a - b;
+		}
+
+		if($scope.lastPage < 7)
+		{
+			for(var i = 1 ; i <= $scope.lastPage ; i++)
+			{
+				$scope.total_page.push(i);
+			}
+		}
+		else if(($scope.pageNo - lenPage >= 1) && ($scope.pageNo + lenPage <= $scope.lastPage))
+		{
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage ; i++)
+			{
+				$scope.total_page.push($scope.pageNo - i);
+				$scope.total_page.push($scope.pageNo + i);
+			}
+
+			$scope.total_page.sort(sortNumber);
+		}
+		else if($scope.pageNo - lenPage < 1)
+		{
+			var tmp = lenPage - ($scope.pageNo - 1);
+
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage + tmp ; i++)
+			{
+				if($scope.pageNo - i >= 1)
+				{
+					$scope.total_page.push($scope.pageNo - i);
+				}
+
+				$scope.total_page.push($scope.pageNo + i);
+			}
+
+			$scope.total_page.sort(sortNumber);
+		}
+		else if($scope.pageNo + lenPage > $scope.lastPage)
+		{
+			var tmp = lenPage - ($scope.lastPage - $scope.pageNo);
+
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage + tmp ; i++)
+			{
+				if($scope.pageNo + i <= $scope.lastPage)
+				{
+					$scope.total_page.push($scope.pageNo + i);
+				}
+
+				$scope.total_page.push($scope.pageNo - i);
+			}
+
+			$scope.total_page.sort(sortNumber);
 		}
 	});
 });
--- public/js/ListDayController.js
+++ public/js/ListDayController.js
@@ -20,12 +20,70 @@ app.controller("ListDayController", function($scope, $http, $routeParams) {
 		$scope.day = date.toISOString()
 
 		$scope.total_page = [];
-		$scope.pageNo = $routeParams.page;
-		$scope.lastPage = response.total_page;
+		$scope.pageNo = parseInt($routeParams.page);
+		$scope.lastPage = parseInt(response.total_page);
 
-		for(var i = 1 ; i <= response.total_page ; i++)
+		var lenPage = 3;
+
+		function sortNumber(a, b) 
 		{
-			$scope.total_page.push(i);
+		    return a - b;
+		}
+
+		if($scope.lastPage < 7)
+		{
+			for(var i = 1 ; i <= $scope.lastPage ; i++)
+			{
+				$scope.total_page.push(i);
+			}
+		}
+		else if(($scope.pageNo - lenPage >= 1) && ($scope.pageNo + lenPage <= $scope.lastPage))
+		{
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage ; i++)
+			{
+				$scope.total_page.push($scope.pageNo - i);
+				$scope.total_page.push($scope.pageNo + i);
+			}
+
+			$scope.total_page.sort(sortNumber);
+		}
+		else if($scope.pageNo - lenPage < 1)
+		{
+			var tmp = lenPage - ($scope.pageNo - 1);
+
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage + tmp ; i++)
+			{
+				if($scope.pageNo - i >= 1)
+				{
+					$scope.total_page.push($scope.pageNo - i);
+				}
+
+				$scope.total_page.push($scope.pageNo + i);
+			}
+
+			$scope.total_page.sort(sortNumber);
+		}
+		else if($scope.pageNo + lenPage > $scope.lastPage)
+		{
+			var tmp = lenPage - ($scope.lastPage - $scope.pageNo);
+
+			$scope.total_page.push($scope.pageNo);
+
+			for(var i = 1 ; i <= lenPage + tmp ; i++)
+			{
+				if($scope.pageNo + i <= $scope.lastPage)
+				{
+					$scope.total_page.push($scope.pageNo + i);
+				}
+
+				$scope.total_page.push($scope.pageNo - i);
+			}
+
+			$scope.total_page.sort(sortNumber);
 		}
 	});
 });
